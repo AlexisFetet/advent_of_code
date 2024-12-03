4b4447fdd021409bfa490871426e8d35b668171b
AlexisFetet <fetet.alexis@gmail.com>
2024-12-03 20:14:54 +0100

    d3

--- src/d3.rs
+++ src/d3.rs
@@ -1,27 +1,49 @@
 use std::fs;
 
+use regex::{self, Regex};
+
 #[derive(Debug)]
 pub struct D3Solver {
-    pub data: i32,
+    pub data: String,
 }
 
 impl D3Solver {
     pub fn extract_info(&mut self, input: String) {
         let contents: String = fs::read_to_string(input)
             .expect("Should have been able to read the file");
+        self.data = contents; 
     }
 
-    pub fn solve_p1(&self) -> u32 {
-        0
+    pub fn solve_p1(&self) -> i32 {
+        let re = Regex::new(r"mul\((?<d1>[0-9]{1,3}),(?<d2>[0-9]{1,3})\)").expect("");
+        re.captures_iter(&self.data).map(|captures| {
+            captures.name("d1").unwrap().as_str().parse::<i32>().unwrap() * captures.name("d2").unwrap().as_str().parse::<i32>().unwrap()
+        }).fold(0, |acc, x| acc + x)
     }
 
-    pub fn solve_p2(&self) -> u32 {
-        0
+    pub fn solve_p2(&self) -> i32 {
+        let mut enabled = true;
+        let re = Regex::new(r"(?<do>do\(\))|(?<dont>don't\(\))|mul\((?<d1>[0-9]{1,3}),(?<d2>[0-9]{1,3})\)").expect("");
+        re.captures_iter(&self.data).map(|captures| {
+            let mut result = 0;
+            if captures.name("dont").is_some() {
+                enabled = false
+            }
+            if captures.name("do").is_some() {
+                enabled = true
+            }
+            result += captures.name("d1").map_or(0, |f| {
+                let temp = f.as_str().parse::<i32>().unwrap() * captures.name("d2").unwrap().as_str().parse::<i32>().unwrap();
+                if enabled {return temp;} else {return 0;}
+            });
+            result
+            
+        }).fold(0, |acc, x| acc + x)
     }
 }
 
 impl Default for D3Solver {
     fn default() -> D3Solver {
-        D3Solver {data : 0}
+        D3Solver {data : String::from("")}
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -26,24 +26,25 @@ fn main() {
     // let mut d2_solver: D2Solver = D2Solver{..Default::default()};
 
     // d2_solver.extract_info(String::from("/home/alexis/advent_of_code/data/d2/test/1.txt"));
-    // println!("Day1 test1 p1: {}", d2_solver.solve_p1());
-    // println!("Day1 test1 p2: {}", d2_solver.solve_p2());
+    // println!("Day2 test1 p1: {}", d2_solver.solve_p1());
+    // println!("Day2 test1 p2: {}", d2_solver.solve_p2());
 
 
     // d2_solver.extract_info(String::from("/home/alexis/advent_of_code/data/d2/input.txt"));
-    // println!("Day1 p1: {}", d2_solver.solve_p1());
-    // println!("Day1 p2: {}", d2_solver.solve_p2());
+    // println!("Day2 p1: {}", d2_solver.solve_p1());
+    // println!("Day2 p2: {}", d2_solver.solve_p2());
 
     // ----------------------------------------------
 
     let mut d3_solver: D3Solver = D3Solver{..Default::default()};
 
     d3_solver.extract_info(String::from("/home/alexis/advent_of_code/data/d3/test/1.txt"));
-    println!("Day1 test1 p1: {}", d3_solver.solve_p1());
-    println!("Day1 test1 p2: {}", d3_solver.solve_p2());
+    println!("Day3 test1 p1: {}", d3_solver.solve_p1());
+    d3_solver.extract_info(String::from("/home/alexis/advent_of_code/data/d3/test/2.txt"));
+    println!("Day3 test2 p2: {}", d3_solver.solve_p2());
 
 
     d3_solver.extract_info(String::from("/home/alexis/advent_of_code/data/d3/input.txt"));
-    println!("Day1 p1: {}", d3_solver.solve_p1());
-    println!("Day1 p2: {}", d3_solver.solve_p2());
+    println!("Day3 p1: {}", d3_solver.solve_p1());
+    println!("Day3 p2: {}", d3_solver.solve_p2());
 }
