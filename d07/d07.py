0a797a12c7a1f33b0bac9e97da4fbe456cc11915
Alexis Fetet <fetet.alexis@gmail.com>
2025-12-07 22:16:11 +0100

    d7

--- d07/d07.py
+++ d07/d07.py
@@ -2,8 +2,27 @@
 This file contains a code attempt for advent of code day 7
 """
 
-
+from copy import deepcopy
 import os
+
+def solve_px(lines):
+    beams = [0] * len(lines[0])
+    beams[lines[0].index('S')] = 1
+    counter = 0
+    for line in lines[1:]:
+        new_beams = deepcopy(beams)
+        if '^' not in line:
+            continue
+        for index, char in enumerate(line):
+            if char == '^':
+                if beams[index] != 0:
+                    counter += 1
+                    new_beams[index - 1] += beams[index]
+                    new_beams[index] = 0
+                    new_beams[index + 1] += beams[index]
+        beams = new_beams
+    return (counter, sum(beams))
+
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
@@ -15,5 +34,5 @@ if __name__ == '__main__':
     with open(f"{day}.txt", "r", encoding="utf-8") as file:
         my_input = file.read().splitlines()
 
-    print(f"Answer part1 : {0}")
-    print(f"Answer part2 : {0}")
+    print(f"Answer exemple (part1, part2) : {solve_px(my_tests)}")
+    print(f"Answer (part1, part2) : {solve_px(my_input)}")
