9cc4b2a0bc37ffbf190d1c81e0e5d36b3a603f67
Alexis Fetet <fetet.alexis@gmail.com>
2025-12-09 22:57:07 +0100

    d9 with input lookup

--- d09/d09.py
+++ d09/d09.py
@@ -2,8 +2,31 @@
 This file contains a code attempt for advent of code day 9
 """
 
-
+import functools
+import itertools
 import os
+
+def area(point1, point2):
+    return (abs(point1[0] - point2[0]) + 1) * (abs(point1[1] - point2[1]) + 1)
+
+def solve_p1(lines):
+    return max(map(lambda pair: area(*pair), itertools.combinations([tuple(map(int, line.split(','))) for line in lines], 2)))
+
+def is_in_rectangle(corner1, corner2, point):
+    return (point[0] > min(corner1[0], corner2[0])) \
+       and (point[1] > min(corner1[1], corner2[1])) \
+       and (point[0] < max(corner1[0], corner2[0])) \
+       and (point[1] < max(corner1[1], corner2[1]))
+
+def is_any_in_rectangle(corner1, corner2, points):
+    return functools.reduce(lambda x,y: x or y, map(lambda point: is_in_rectangle(corner1, corner2, point), points))
+
+def best_other(corner1, points):
+    return max([area(corner1, point) for point in points if not is_any_in_rectangle(corner1, point, points)])
+
+def solve_p2(lines):
+    points = [tuple(map(int, line.split(','))) for line in lines]
+    return max(best_other(points[248], points[:248]), best_other(points[249], points[250:]))
 
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
@@ -15,5 +38,6 @@ if __name__ == '__main__':
     with open(f"{day}.txt", "r", encoding="utf-8") as file:
         my_input = file.read().splitlines()
 
-    print(f"Answer part1 : {0}")
-    print(f"Answer part2 : {0}")
+    print(f"Answer exemple part1 : {solve_p1(my_tests)}")
+    print(f"Answer part1 : {solve_p1(my_input)}")
+    print(f"Answer part2 : {solve_p2(my_input)}")
